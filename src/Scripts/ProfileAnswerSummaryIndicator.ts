6d3d299637fdc23dcce8c63856139f6e35392b66
Henry Ecker <HenryEcker@users.noreply.github.com>
2022-10-12 21:44:35 -0400

    Remove client-side caching of answer summary annotations

--- src/Scripts/ProfileAnswerSummaryIndicator.ts
+++ src/Scripts/ProfileAnswerSummaryIndicator.ts
@@ -8,48 +8,31 @@ const getAnswerIdsOnPage = (): Set<number> => {
     }).toArray());
 };
 
-const setDifference = <T>(a: Set<T>, b: Set<T>): Set<T> => {
-    return new Set([...a].filter(i => !b.has(i)));
-};
+// const setDifference = <T>(a: Set<T>, b: Set<T>): Set<T> => {
+//     return new Set([...a].filter(i => !b.has(i)));
+// };
+
+// const mergeSets = <T>(a: Set<T>, b: Set<T>): Set<T> => {
+//     return new Set([...a, ...b]);
+// };
+
 const setIntersection = <T>(a: Set<T>, b: Set<T>): Set<T> => {
     return new Set([...a].filter(i => b.has(i)));
 };
 
-const mergeSets = <T>(a: Set<T>, b: Set<T>): Set<T> => {
-    return new Set([...a, ...b]);
-};
-
 
 class SummaryAnnotator {
-    private checkedPostIds: Set<number>;
-    private annotatedPosts: Set<number>;
-
-    constructor() {
-        this.checkedPostIds = new Set();
-        this.annotatedPosts = new Set();
-    }
-
-    updateSets() {
+    annotateAnswers() {
         const postIdsOnPage = getAnswerIdsOnPage();
-        const uncheckedIds = [...setDifference(postIdsOnPage, this.checkedPostIds)];
-        if (uncheckedIds.length === 0) {
-            this.render(postIdsOnPage);
-            return; // don't fetch more if we've already seen all the IDs on the page
-        }
-        void getSummaryPostInfoFromIds(
-            // Only search for ids on the page that we haven't already checked
-            uncheckedIds
-        ).then(postResults => {
-            this.annotatedPosts = mergeSets(this.annotatedPosts, postResults);
-            this.checkedPostIds = mergeSets(this.checkedPostIds, postIdsOnPage);
-
-            this.render(postIdsOnPage);
-        });
+        void getSummaryPostInfoFromIds([...postIdsOnPage])
+            .then(postResults => {
+                this.render(postIdsOnPage, new Set<number>(postResults));
+            });
     }
 
-    private render(postsOnPage: Set<number>) {
+    private render(postsOnPage: Set<number>, annotatedPosts: Set<number>) {
         // Only display indicator for posts that are both on the page and should be annotated
-        for (const postId of setIntersection(postsOnPage, this.annotatedPosts)) {
+        for (const postId of setIntersection(postsOnPage, annotatedPosts)) {
 
             $(`#answer-id-${postId} .s-post-summary--stats-item:eq(0)`)
                 .before($(`<div title="This post is noted in the Case Manager System" class="s-post-summary--stats-item" style="color: var(--red-600)">${buildCaseSvg()}</div>`));
@@ -60,13 +43,13 @@ class SummaryAnnotator {
 
 export const buildAnswerSummaryIndicator = () => {
     const summaryAnnotator = new SummaryAnnotator();
-    summaryAnnotator.updateSets();
+    summaryAnnotator.annotateAnswers();
     // The escape \\ is for the ? that comes with the variable
     const matchPattern = new RegExp(`users/tab/\\d+\\${userAnswerTabProfile}`, 'gi');
     // Handle buttons on tab/page navigation
     $(document).on('ajaxComplete', (_0, _1, {url}) => {
         if (url.match(matchPattern)) {
-            summaryAnnotator.updateSets();
+            summaryAnnotator.annotateAnswers();
         }
     });
 };
