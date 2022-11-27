1f090825a961f6d7a63117f56126834118596648
HenryEcker <HenryEcker@users.noreply.github.com>
2022-11-27 02:11:53 -0500

    Add mild compression to "unminified" version

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,25 @@ import buildConfig from './vite-shared-config';
 const fullConfig = buildConfig('SOCaseManager.user.js');
 // Runs first so make a clean directory
 fullConfig['build']['emptyOutDir'] = true;
-// Do not minify at all
-fullConfig['build']['minify'] = false;
+
+// Minify config
+fullConfig['build']['minify'] = 'terser';
+fullConfig['build']['terserOptions'] = {
+    compress: {
+        defaults: false,
+        properties: true,
+        side_effects: true,
+        evaluate: true,
+        if_return: true,
+        keep_classnames: true,
+        keep_fnames: true
+    },
+    format: {
+        braces: true,
+        beautify: true,
+        comments: false
+    },
+    mangle: false
+};
 
 export default fullConfig;
