15f990727a4b2bd87843de033ed05a5e6191be26
Henry Ecker <HenryEcker@users.noreply.github.com>
2022-11-29 20:43:35 -0500

    Expand search param key replacements

--- vite-shared-config.js
+++ vite-shared-config.js
@@ -9,6 +9,13 @@ const authRedirectUri = `${awsApiRoute}/auth/se/oauth`;
 export const seTokenAuthRoute = `https://stackoverflow.com/oauth?client_id=24380&scope=no_expiry&redirect_uri=${authRedirectUri}`;
 
 const defObj = {
+    searchParamKeys: {
+        page: 'page',
+        group: 'group',
+        search: 'search',
+        tableFilter: 'table-filter',
+        tab: 'tab', // Technically "tab" is consumed as a search param key, but its usage is specified below
+    },
     tabIdentifiers: {
         settings: '?tab=case-manager-settings',
         userSummary: '?tab=case-manager',
@@ -23,10 +30,6 @@ const defObj = {
         seAPIBase: 'https://api.stackexchange.com/2.3',
         apiKey: 'BkvRpNB*IzKMdjAcikc4jA(('
     },
-    caseManagerControlPanelSearchKey: {
-        pageKey: 'page',
-        tableFilterKey: 'table-filter'
-    }
 };
 
 export default (fileName) => {
