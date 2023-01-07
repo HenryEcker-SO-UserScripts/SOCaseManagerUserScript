d6a00a7e07da00354cd8e3a9c597b7bd7adbebb3
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-01-07 18:03:19 -0500

    Simplify action summary icon build logic

--- src/Scripts/ProfileAnswerSummaryIndicator.ts
+++ src/Scripts/ProfileAnswerSummaryIndicator.ts
@@ -1,58 +1,48 @@
 import {getSummaryPostActionsFromIds, type SummaryPostActionResponse} from '../AWSAPI';
-import {buildCaseSvg, buildCheckmarkSvg, buildEditPenSvg, buildAlertSvg} from '../SVGBuilders';
+import {buildAlertSvg, buildCaseSvg, buildCheckmarkSvg, buildEditPenSvg} from '../SVGBuilders';
 
 
-const getAnswerIdsOnPage = (): Set<number> => {
+const getAnswerIdsOnPage = (): Set<string> => {
     return new Set($('.s-post-summary').map((i, e) => {
-        return Number(e.getAttribute('data-post-id'));
+        return e.getAttribute('data-post-id');
     }).toArray());
 };
 
-// const setDifference = <T>(a: Set<T>, b: Set<T>): Set<T> => {
-//     return new Set([...a].filter(i => !b.has(i)));
-// };
-
-// const mergeSets = <T>(a: Set<T>, b: Set<T>): Set<T> => {
-//     return new Set([...a, ...b]);
-// };
-
-const setIntersection = <T>(a: Set<T>, b: Set<T>): Set<T> => {
-    return new Set([...a].filter(i => b.has(i)));
-};
-
-
 class SummaryAnnotator {
+    private static iconAttrMap: {
+        [id: number]: {
+            desc: string;
+            colourVar: string;
+            svg: string;
+        };
+    } = {
+        1: {desc: 'Looks OK', colourVar: '--green-600', svg: buildCheckmarkSvg(16)},
+        2: {desc: 'edited', colourVar: '--green-800', svg: buildEditPenSvg(16)},
+        3: {desc: 'plagiarised', colourVar: '--red-600', svg: buildCaseSvg(16)},
+        5: {desc: 'suspicious', colourVar: '--yellow-700', svg: buildAlertSvg(16)},
+    };
+
     annotateAnswers() {
         const postIdsOnPage = getAnswerIdsOnPage();
         void getSummaryPostActionsFromIds([...postIdsOnPage])
             .then(postResults => {
-                this.render(postIdsOnPage, postResults);
+                this.render(postResults);
             });
     }
 
-    private render(postsOnPage: Set<number>, annotatedPosts: SummaryPostActionResponse) {
-        // Only display indicator for posts that are both on the page and should be annotated
-        for (const postId of setIntersection(postsOnPage, new Set(Object.keys(annotatedPosts).map(Number)))) {
-            const eventValues = annotatedPosts[postId];
-            const symbolBar = $('<div class="case-manager-symbol-group d-flex fd-row g2"></div>');
-            eventValues.forEach(e => {
-                switch (e) {
-                    case 1:
-                        symbolBar.append($(`<div title="This post is noted in the Case Manager System as Looks OK" class="flex--item s-post-summary--stats-item" style="color: var(--green-600)">${buildCheckmarkSvg()}</div>`));
-                        break;
-                    case 2:
-                        symbolBar.append($(`<div title="This post is noted in the Case Manager System as edited" class="flex--item s-post-summary--stats-item" style="color: var(--green-800)">${buildEditPenSvg()}</div>`));
-                        break;
-                    case 3:
-                        symbolBar.append($(`<div title="This post is noted in the Case Manager System as plagiarised" class="flex--item s-post-summary--stats-item" style="color: var(--red-600)">${buildCaseSvg()}</div>`));
-                        break;
-                    case 5:
-                        symbolBar.append($(`<div title="This post is noted in the Case Manager System as suspicious" class="flex--item s-post-summary--stats-item" style="color: var(--yellow-700)">${buildAlertSvg()}</div>`));
-                        break;
-                }
+    private render(annotatedPosts: SummaryPostActionResponse) {
+        Object
+            .entries(annotatedPosts)
+            .forEach(([postId, eventValues]) => {
+                const symbolBar = $('<div class="case-manager-symbol-group d-flex fd-row g2 ba bar-sm p2"></div>');
+                eventValues.forEach(e => {
+                    if (Object.hasOwn(SummaryAnnotator.iconAttrMap, e)) {
+                        const {desc, colourVar, svg} = SummaryAnnotator.iconAttrMap[e];
+                        symbolBar.append($(`<div title="This post is noted in the Case Manager System as ${desc}" class="flex--item s-post-summary--stats-item" style="color: var(${colourVar})">${svg}</div>`));
+                    }
+                });
+                $(`#answer-id-${postId} .s-post-summary--stats-item:eq(0)`).before(symbolBar);
             });
-            $(`#answer-id-${postId} .s-post-summary--stats-item:eq(0)`).before(symbolBar);
-        }
     }
 }
 
