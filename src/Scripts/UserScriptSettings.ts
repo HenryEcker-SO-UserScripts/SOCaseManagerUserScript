818a4b19f88b946f53c90eeb377c05e312f28caf
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-01-08 14:43:21 -0500

    Don't wait for tokens to complete fetch before rendering other settings components

--- src/Scripts/UserScriptSettings.ts
+++ src/Scripts/UserScriptSettings.ts
@@ -6,125 +6,131 @@ import {buildNukeOptionControls, hasCheckedChild} from './ModTools';
 
 declare const StackExchange: StackExchangeAPI;
 
-export const buildUserScriptSettingsPanel = async () => {
-    const tokens: string[] = await fetchFromAWS('/auth/credentials').then(res => res.json());
 
-    const container = $('<div></div>');
-    container.append('<div class="s-page-title mb24"><h1 class="s-page-title--header m0 baw0 p0">Case Manager UserScript Settings</h1></div>');
-    const toolGrid = $('<div class="d-grid grid__2 md:grid__1 g32"></div>');
-    container.append(toolGrid);
+const buildExistingTokensControls = (): JQuery => {
+    const existingTokensComponent = $('<div></div>');
 
-    {
-        const existingTokensComponent = $('<div></div>');
-        toolGrid.append(existingTokensComponent);
+    existingTokensComponent.append('<h3 class="fs-title mb12">Existing Auth Tokens</h3>');
+    const tokenList = $('<div></div>');
+    existingTokensComponent.append(tokenList);
 
-        existingTokensComponent.append('<h3 class="fs-title mb12">Existing Auth Tokens</h3>');
-        const tokenList = $('<div></div>');
-        existingTokensComponent.append(tokenList);
+    void fetchFromAWS('/auth/credentials')
+        .then(res => res.json())
+        .then((tokens: string[]) => {
+            // Token Invalidator
+            tokens.forEach((token) => {
+                const tokenRow = $('<div class="d-flex fd-row ai-center"></div>');
+                tokenList.append(tokenRow);
 
-        // Token Invalidator
-        tokens.forEach((token) => {
-            const tokenRow = $('<div class="d-flex fd-row ai-center"></div>');
-            tokenList.append(tokenRow);
-
-            tokenRow.append(`<span>${token}</span>`);
-            const invalidateButton = $('<button class="s-btn s-btn__danger">Invalidate</button>');
-            invalidateButton.on('click', (ev) => {
-                ev.preventDefault();
-                void fetchFromAWS(`/auth/credentials/${token}/invalidate`).then((res) => {
-                    if (res.status === 200) {
-                        // Get rid of the row from the table
-                        tokenRow.remove();
-                        if (GM_getValue(seApiToken) === token) {
-                            // If invalidating the local storage key also remove from GM storage
-                            GM_deleteValue(seApiToken);
-                            GM_deleteValue(accessToken);
-                            window.location.reload();
-                        }
-                    }
-                });
-            });
-            tokenRow.append(invalidateButton);
-        });
-
-        const deAuthoriseButton = $('<button class="s-btn s-btn__outlined s-btn__danger mt16" id="app-24380">De-authenticate Application</button>');
-        existingTokensComponent.append(deAuthoriseButton);
-
-        deAuthoriseButton.on('click', (ev) => {
-            ev.preventDefault();
-            void StackExchange.helpers.showConfirmModal(
-                {
-                    title: 'De-authenticate this Application',
-                    bodyHtml: '<p>Are you sure you want to de-authenticate this application? All existing access tokens will be invalidated and removed from storage. This app will no longer appear in your authorized applications list. You will no longer be able to use any existing access tokens and will need to reauthenticate to continue use.</p><p><b>Note:</b> All of your actions will be retained and associated to your user id even after de-authenticating. You may resume access at any time by authorising the application again.</p>',
-                    buttonLabel: 'De-authenticate',
-                }
-            ).then((confirm: boolean) => {
-                if (confirm) {
-                    void fetchFromAWS(`/auth/credentials/${GM_getValue(seApiToken)}/de-authenticate`)
-                        .then((res) => {
-                            if (res.status === 200) {
-                                // These are all now no longer valid
+                tokenRow.append(`<span>${token}</span>`);
+                const invalidateButton = $('<button class="s-btn s-btn__danger">Invalidate</button>');
+                invalidateButton.on('click', (ev) => {
+                    ev.preventDefault();
+                    void fetchFromAWS(`/auth/credentials/${token}/invalidate`).then((res) => {
+                        if (res.status === 200) {
+                            // Get rid of the row from the table
+                            tokenRow.remove();
+                            if (GM_getValue(seApiToken) === token) {
+                                // If invalidating the local storage key also remove from GM storage
                                 GM_deleteValue(seApiToken);
                                 GM_deleteValue(accessToken);
                                 window.location.reload();
                             }
-                        });
-                }
+                        }
+                    });
+                });
+                tokenRow.append(invalidateButton);
             });
         });
-    }
 
-    // Token Issuer
-    {
-        const getNewToken = $('<div></div>');
-        toolGrid.append(getNewToken);
+    const deAuthoriseButton = $('<button class="s-btn s-btn__outlined s-btn__danger mt16" id="app-24380">De-authenticate Application</button>');
+    existingTokensComponent.append(deAuthoriseButton);
 
-        getNewToken.append('<h3 class="fs-title mb12">Issue new token</h3>');
-        getNewToken.append('<p>You can issue a new auth token for use on another device or to manually replace an existing token. Please invalidate any existing tokens, so they can no longer be used to access your information.</p>');
-        getNewToken.append(`<a class="s-link s-link__underlined" href="${seTokenAuthRoute}" target="_blank" rel="noopener noreferrer">Issue new auth token</a>`);
-    }
+    deAuthoriseButton.on('click', (ev) => {
+        ev.preventDefault();
+        void StackExchange.helpers.showConfirmModal(
+            {
+                title: 'De-authenticate this Application',
+                bodyHtml: '<p>Are you sure you want to de-authenticate this application? All existing access tokens will be invalidated and removed from storage. This app will no longer appear in your authorized applications list. You will no longer be able to use any existing access tokens and will need to reauthenticate to continue use.</p><p><b>Note:</b> All of your actions will be retained and associated to your user id even after de-authenticating. You may resume access at any time by authorising the application again.</p>',
+                buttonLabel: 'De-authenticate',
+            }
+        ).then((confirm: boolean) => {
+            if (confirm) {
+                void fetchFromAWS(`/auth/credentials/${GM_getValue(seApiToken)}/de-authenticate`)
+                    .then((res) => {
+                        if (res.status === 200) {
+                            // These are all now no longer valid
+                            GM_deleteValue(seApiToken);
+                            GM_deleteValue(accessToken);
+                            window.location.reload();
+                        }
+                    });
+            }
+        });
+    });
+    return existingTokensComponent;
+};
 
+const buildTokenIssuer = (): JQuery => {
+    const getNewToken = $('<div></div>');
 
-    // Flag base template manager
+    getNewToken.append('<h3 class="fs-title mb12">Issue new token</h3>');
+    getNewToken.append('<p>You can issue a new auth token for use on another device or to manually replace an existing token. Please invalidate any existing tokens, so they can no longer be used to access your information.</p>');
+    getNewToken.append(`<a class="s-link s-link__underlined" href="${seTokenAuthRoute}" target="_blank" rel="noopener noreferrer">Issue new auth token</a>`);
+    return getNewToken;
+};
+
+const buildNukeConfigControls = (): JQuery => {
+    const nukePostConfig: CmNukePostConfig = JSON.parse(GM_getValue(nukePostOptions, nukePostDefaultConfigString));
+    const templateIssuer = $('<div></div>');
+
+    templateIssuer.append('<h3 class="fs-title mb12">Edit base options for nuking posts</h3>');
+    const templateForm = $('<form class="d-flex fd-column g8"></form>');
+    const {
+        textareaLabel,
+        textarea,
+        checkboxContainer,
+        shouldFlagCheckbox,
+        shouldCommentCheckbox,
+        shouldLogCheckbox
+    } = buildNukeOptionControls('nuke-config', nukePostConfig);
+    templateForm.append(textareaLabel);
+    templateForm.append(textarea);
+
+    templateForm.append(checkboxContainer);
+
+    templateForm.append('<div><button class="s-btn s-btn__primary" type="submit">Save Config</button></div>');
+
+    const formHandler = (ev: JQuery.Event) => {
+        ev.preventDefault();
+        nukePostConfig.detailText = (textarea.val() as string | undefined) || '';
+        nukePostConfig.flag = hasCheckedChild(shouldFlagCheckbox);
+        nukePostConfig.comment = hasCheckedChild(shouldCommentCheckbox);
+        nukePostConfig.log = hasCheckedChild(shouldLogCheckbox);
+        GM_setValue(nukePostOptions, JSON.stringify(nukePostConfig));
+
+        StackExchange.helpers.showToast('Config updated successfully!', {
+            type: 'success',
+            transientTimeout: 3000
+        });
+    };
+    templateForm.on('submit', formHandler);
+
+    templateIssuer.append(templateForm);
+    return templateIssuer;
+};
+
+export const buildUserScriptSettingsPanel = () => {
+    const container = $('<div class="s-page-title mb24"><h1 class="s-page-title--header m0 baw0 p0">Case Manager UserScript Settings</h1></div>');
+    const toolGrid = $('<div class="d-grid grid__2 md:grid__1 g32"></div>');
+
+    toolGrid.append(buildExistingTokensControls());
+    toolGrid.append(buildTokenIssuer());
     if (StackExchange.options.user.isModerator) {
-        const nukePostConfig: CmNukePostConfig = JSON.parse(GM_getValue(nukePostOptions, nukePostDefaultConfigString));
-        const templateIssuer = $('<div></div>');
-        toolGrid.append(templateIssuer);
-
-        templateIssuer.append('<h3 class="fs-title mb12">Edit base options for nuking posts</h3>');
-        const templateForm = $('<form class="d-flex fd-column g8"></form>');
-        const {
-            textareaLabel,
-            textarea,
-            checkboxContainer,
-            shouldFlagCheckbox,
-            shouldCommentCheckbox,
-            shouldLogCheckbox
-        } = buildNukeOptionControls('nuke-config', nukePostConfig);
-        templateForm.append(textareaLabel);
-        templateForm.append(textarea);
-
-        templateForm.append(checkboxContainer);
-
-        templateForm.append('<div><button class="s-btn s-btn__primary" type="submit">Save Config</button></div>');
-
-        const formHandler = (ev: JQuery.Event) => {
-            ev.preventDefault();
-            nukePostConfig.detailText = (textarea.val() as string | undefined) || '';
-            nukePostConfig.flag = hasCheckedChild(shouldFlagCheckbox);
-            nukePostConfig.comment = hasCheckedChild(shouldCommentCheckbox);
-            nukePostConfig.log = hasCheckedChild(shouldLogCheckbox);
-            GM_setValue(nukePostOptions, JSON.stringify(nukePostConfig));
-
-            StackExchange.helpers.showToast('Config updated successfully!', {
-                type: 'success',
-                transientTimeout: 3000
-            });
-        };
-        templateForm.on('submit', formHandler);
-
-        templateIssuer.append(templateForm);
+        toolGrid.append(buildNukeConfigControls());
     }
 
-    return container;
+    return $(document.createDocumentFragment())
+        .append(container)
+        .append(toolGrid);
 };
