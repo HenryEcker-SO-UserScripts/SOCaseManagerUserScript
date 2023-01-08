a0cc5278b96cebf8a72fee31e667ff393840a7e5
HenryEcker <hde1997@gmail.com>
2023-01-07 19:41:52 -0500

    Only display flag/comment template manager if is a mod (since only availbe in mod tools)

--- dist/SOCaseManager.user.js
+++ dist/SOCaseManager.user.js
@@ -3,7 +3,7 @@
 // @description Help facilitate and track collaborative plagiarism cleanup efforts
 // @homepage    https://github.com/HenryEcker/SOCaseManagerUserScript
 // @author      Henry Ecker (https://github.com/HenryEcker)
-// @version     0.1.12
+// @version     0.1.13
 // @downloadURL https://github.com/HenryEcker/SOCaseManagerUserScript/raw/master/dist/SOCaseManager.user.js
 // @updateURL   https://github.com/HenryEcker/SOCaseManagerUserScript/raw/master/dist/SOCaseManager.user.js
 // @match       *://stackoverflow.com/questions/*
@@ -1022,7 +1022,7 @@
             getNewToken.append("<p>You can issue a new auth token for use on another device or to manually replace an existing token. Please invalidate any existing tokens, so they can no longer be used to access your information.</p>");
             getNewToken.append(`<a class="s-link s-link__underlined" href="${seTokenAuthRoute}" target="_blank" rel="noopener noreferrer">Issue new auth token</a>`);
         }
-        {
+        if (StackExchange.options.user.isModerator) {
             const templateIssuer = $("<div></div>");
             toolGrid.append(templateIssuer);
             templateIssuer.append('<h3 class="fs-title mb12">Edit base message template for comment/flags</h3>');
