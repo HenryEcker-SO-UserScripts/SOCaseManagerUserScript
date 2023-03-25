0da8f1b26bf4b10be71d459d01d67c6451332fe1
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-03-24 22:29:46 -0400

    Install and migrate to my UserScript utility functions

--- src/API/SEAPI.ts
+++ src/API/SEAPI.ts
@@ -1,21 +1,5 @@
 import {seApiToken} from './gmAPI';
 
-// Currently relying on 200 status code since it sometimes returns an HTML response and sometimes JSON
-// Depending on if the CM escalate message was displayed or not.
-// export interface PostDeleteAsPlagiarismResponse {
-//     success: boolean;
-//     message: string;
-// }
-
-export interface FlagPlagiarismResponse {
-    FlagType: number;
-    Message: string;
-    Outcome: number;
-    ResultChangedState: boolean;
-    Success: boolean;
-}
-
-
 export interface SEAPIResponse<T> {
     has_more: boolean;
     items: T[];
